cb526ba3ef7b8bcac33ba1cd87eea8c6c3a78b16
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-12-04 13:42:02 -0600

    Create settings.py

---
+++ talent/settings.py
@@ -0,0 +1,14 @@
+import pathlib
+
+DIR_ROOT = pathlib.Path(__file__).resolve().parent.parent
+DIR_DATA = DIR_ROOT / "data"
+DIR_SDE = DIR_DATA / "sde"
+
+SDE_LOCAL_MD5 = DIR_SDE / "local.md5"
+SDE_CATEGORIES_CSV = DIR_SDE / "categories.csv"
+SDE_GROUPS_CSV = DIR_SDE / "groups.csv"
+SDE_MARKET_GROUPS_CSV = DIR_SDE / "market_groups.csv"
+SDE_META_GROUPS_CSV = DIR_SDE / "meta_groups.csv"
+SDE_META_TYPES_CSV = DIR_SDE / "meta_types.csv"
+SDE_RACES_CSV = DIR_SDE / "races.csv"
+SDE_TYPES_CSV = DIR_SDE / "types.csv"

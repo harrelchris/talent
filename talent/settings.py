f88770d6071b07fc32e8b214d3283be5e2c9e5ee
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-12-04 21:40:41 -0600

    Read .env

--- talent/settings.py
+++ talent/settings.py
@@ -1,11 +1,16 @@
+import os
 import pathlib
+import dotenv
 
 DIR_ROOT = pathlib.Path(__file__).resolve().parent.parent
 DIR_DATA = DIR_ROOT / "data"
 DIR_ESI = DIR_DATA / "esi"
 DIR_SDE = DIR_DATA / "sde"
 
+dotenv.load_dotenv(DIR_ROOT / ".env")
+
 ESI_JITA_ORDERS_CSV = DIR_ESI / "jita_orders.csv"
+ESI_VALE_ORDERS_CSV = DIR_ESI / "vale_orders.csv"
 
 SDE_LOCAL_MD5 = DIR_SDE / "local.md5"
 SDE_CATEGORIES_CSV = DIR_SDE / "categories.csv"
@@ -15,3 +20,8 @@ SDE_META_GROUPS_CSV = DIR_SDE / "meta_groups.csv"
 SDE_META_TYPES_CSV = DIR_SDE / "meta_types.csv"
 SDE_RACES_CSV = DIR_SDE / "races.csv"
 SDE_TYPES_CSV = DIR_SDE / "types.csv"
+
+JWT_PATH = DIR_ROOT / 'jwt.json'
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CALLBACK_URL = os.environ.get('CALLBACK_URL')
+SCOPE = os.environ.get('SCOPE')

112808712e75e73edcf0ac59ce7021896a3ad1c3
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-12-04 21:25:29 -0600

    Create utils.py

---
+++ talent/utils.py
@@ -0,0 +1,30 @@
+import csv
+import pathlib
+import settings
+
+
+def get_type_ids() -> set:
+    types = load_csv(settings.SDE_TYPES_CSV)
+    return set(types.keys())
+
+
+def load_csv(path: pathlib.Path) -> dict:
+    output = {}
+    with path.open("r", encoding="utf8") as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            record = dict(zip(header, row))
+            cast_ids(record)
+            record_id = record.pop("type_id")
+            output[record_id] = record
+    return output
+
+
+def cast_ids(data: dict):
+    for key, value in data.items():
+        if "id" in key:
+            try:
+                data[key] = int(value)
+            except ValueError:
+                pass

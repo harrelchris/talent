f4cb0293be4a600dab58f8e4a39681495c057a27
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-12-04 21:41:08 -0600

    Add orders.csv path

--- talent/settings.py
+++ talent/settings.py
@@ -5,12 +5,15 @@ import dotenv
 DIR_ROOT = pathlib.Path(__file__).resolve().parent.parent
 DIR_DATA = DIR_ROOT / "data"
 DIR_ESI = DIR_DATA / "esi"
+DIR_ETL = DIR_DATA / "etl"
 DIR_SDE = DIR_DATA / "sde"
 
 dotenv.load_dotenv(DIR_ROOT / ".env")
 
 ESI_JITA_ORDERS_CSV = DIR_ESI / "jita_orders.csv"
 ESI_VALE_ORDERS_CSV = DIR_ESI / "vale_orders.csv"
+
+ETL_ORDERS_CSV = DIR_ETL / "orders.csv"
 
 SDE_LOCAL_MD5 = DIR_SDE / "local.md5"
 SDE_CATEGORIES_CSV = DIR_SDE / "categories.csv"

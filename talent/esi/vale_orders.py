0cc2addc4d26386a272d714532dc2e233a9a42d6
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-12-04 21:39:53 -0600

    Create vale_orders.csv

---
+++ talent/esi/vale_orders.py
@@ -0,0 +1,37 @@
+import concurrent.futures
+import csv
+
+import evesso
+from requests_futures import sessions
+from talent import settings
+
+structure_id = 1035466617946
+
+session = sessions.FuturesSession()
+
+sso = evesso.SSO(
+    client_id=settings.CLIENT_ID,
+    callback_url=settings.CALLBACK_URL,
+    scope=settings.SCOPE,
+    jwt_file_path=settings.JWT_PATH,
+)
+sso_header = sso.get_header()
+
+url = "https://esi.evetech.net/latest/markets/structures/{structure_id}/?datasource=tranquility&page={page}"
+request = session.head(url.format(structure_id=structure_id, page=1), headers=sso_header)
+response = request.result()
+page_count = int(response.headers.get("X-Pages", 0))
+
+urls = [url.format(structure_id=structure_id, page=page) for page in range(1, page_count + 1)]
+futures = [session.get(url=url, headers=sso_header) for url in urls]
+
+records = []
+for future in concurrent.futures.as_completed(futures):
+    response = future.result()
+    content = response.json()
+    records.extend(content)
+
+with settings.ESI_VALE_ORDERS_CSV.open("w", newline="\n", encoding="utf8") as f:
+    writer = csv.writer(f)
+    writer.writerow(records[0].keys())
+    writer.writerows([r.values() for r in records])

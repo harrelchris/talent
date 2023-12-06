fc9598f5a984488ee03a02e7d5b56b4012d637e2
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-12-05 19:51:37 -0600

    Enclose script in main function

--- talent/esi/vale_orders.py
+++ talent/esi/vale_orders.py
@@ -15,21 +15,27 @@ sso = evesso.SSO(
 )
 sso_header = sso.get_header()
 
-url = "https://esi.evetech.net/latest/markets/structures/1035466617946/?datasource=tranquility&page={page}"
-request = session.head(url.format(page=1), headers=sso_header)
-response = request.result()
-page_count = int(response.headers.get("X-Pages", 0))
 
-urls = [url.format(page=page) for page in range(1, page_count + 1)]
-futures = [session.get(url=url, headers=sso_header) for url in urls]
+def main():
+    url = "https://esi.evetech.net/latest/markets/structures/1035466617946/?datasource=tranquility&page={page}"
+    request = session.head(url.format(page=1), headers=sso_header)
+    response = request.result()
+    page_count = int(response.headers.get("X-Pages", 0))
 
-records = []
-for future in concurrent.futures.as_completed(futures):
-    response = future.result()
-    content = response.json()
-    records.extend(content)
+    urls = [url.format(page=page) for page in range(1, page_count + 1)]
+    futures = [session.get(url=url, headers=sso_header) for url in urls]
 
-with settings.ESI_VALE_ORDERS_CSV.open("w", newline="\n", encoding="utf8") as f:
-    writer = csv.writer(f)
-    writer.writerow(records[0].keys())
-    writer.writerows([r.values() for r in records])
+    records = []
+    for future in concurrent.futures.as_completed(futures):
+        response = future.result()
+        content = response.json()
+        records.extend(content)
+
+    with settings.ESI_VALE_ORDERS_CSV.open("w", newline="\n", encoding="utf8") as f:
+        writer = csv.writer(f)
+        writer.writerow(records[0].keys())
+        writer.writerows([r.values() for r in records])
+
+
+if __name__ == "__main__":
+    main()
